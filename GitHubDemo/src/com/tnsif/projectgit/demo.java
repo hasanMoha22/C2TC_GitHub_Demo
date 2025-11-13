f126dd707c6f3ac4aae797fc912b74c4787a7848
hamjath <syedanwar@gmail.com>
2025-11-13 14:31:36 +0530

    GitDemo Done

---
+++ GitHubDemo/src/com/tnsif/projectgit/demo.java
@@ -0,0 +1,12 @@
+package com.tnsif.projectgit;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
